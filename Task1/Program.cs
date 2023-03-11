2008575a71a9cdee83ab35572a4ecc8f99cfbe5e
Anton Abdrakhmanov <ode15s@yandex.ru>
2023-03-11 23:03:08 +0300

    Task1: task is done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,35 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+int InputInt32(string message)
+{
+    System.Console.Write(($"{message}: "));
+    int value;
+    bool isCorrectInt32 = int.TryParse(Console.ReadLine(), out value);
+    if (isCorrectInt32 && value > 0)
+    {
+        return value;
+    }
+    System.Console.WriteLine("You entered not a number or negative number");
+    Environment.Exit(-1);
+    return 0;
+}
+
+void PrintNaturalNumber(int StartNumber, int FinishNumber)
+{
+
+    if (StartNumber > FinishNumber) return;
+    StartNumber = StartNumber + StartNumber % 2;//если StartNumber нечетное, то начнем вывод чисел со следующего за ним четного. А если StartNumber четное, то с него и начинаем. 
+    System.Console.Write($"{StartNumber} ");
+    PrintNaturalNumber(StartNumber + 2, FinishNumber);
+
+}
+
+int StartNum = InputInt32("Enter Start number");
+int EndNum = InputInt32("Enter End number");
+if (StartNum < EndNum)
+{
+    System.Console.Write($"All even number in range [{StartNum}..{EndNum}]: ");
+    PrintNaturalNumber(StartNum, EndNum);
+}
+else System.Console.WriteLine("Error! End number must be  > StartNumber! ");
